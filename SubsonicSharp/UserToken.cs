10ee8946e4ad956fa3dfcbe276a2c102302117d6
Christopher Davis <thechristopheriandavis@gmail.com>
2016-04-30 19:56:55 -0400

    User authentication token info class created and tests added

---
+++ SubsonicSharp/UserToken.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xBrainLab.Security.Cryptography;
+
+namespace SubsonicSharp
+{
+    class UserToken
+    {
+        public string Username { get; set; }
+        public bool Plaintext { get; set; }
+        public string Token { get; set; }
+        public string Salt { get; set; }
+
+
+        public UserToken(string username, string password, bool plaintext = false, int saltLength = 6)
+        {
+            Plaintext = plaintext;
+            Username = username;
+            if (Plaintext)
+            {
+                Token = password;
+            }
+            else
+            {
+                Salt = GenerateSalt(saltLength);
+                Token = GenerateMd5(password + Salt);
+            }
+        }
+
+        //Generates the MD5 hash for a given string
+        internal static string GenerateMd5(string text)
+        {
+            string hash = MD5.GetHashString(text);
+            //Subsonic expects letters to be lowercase
+            return hash.ToLower();
+        }
+
+        //Generates the salt needed for hashed password
+        internal static string GenerateSalt(int length)
+        {
+            const string valid = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+            //As this token is sent in clear text anyway, it hardly seems worth using a cryptographic RNG with a 300x performance hit
+            Random rand = new Random();
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = valid[rand.Next(valid.Length)];
+            }
+            return new string(chars);
+        }
+
+        internal void UpdatePassword(string password)
+        {
+            Token = GenerateMd5(password + Salt);
+        }
+
+        public override string ToString()
+        {
+            if (Plaintext)
+                return $"u={Username}&p={Token}";
+            else
+            {
+                return $"u={Username}&t={Token}&s={Salt}";
+            }
+        }
+    }
+}
---
+++ Tests/UserTokenTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SubsonicSharp;
+
+namespace Tests
+{
+    [TestClass]
+    public class UserTokenTests
+    {
+        [TestMethod]
+        public void Md5Test()
+        {
+            string hash = UserToken.GenerateMd5("sesamec19b2d");
+            string expected = "26719a1196d2a940705a59634eb18eab";
+            Assert.AreEqual(expected,hash);
+        }
+
+        [TestMethod]
+        public void PlainTextPasswordStringTest()
+        {
+            UserToken test = new UserToken("test", "password", true);
+            string testString = test.ToString();
+            string expected = "u=test&p=password";
+            Assert.AreEqual(expected, testString);
+        }
+
+        [TestMethod]
+        public void HashedPasswordStringTest()
+        {
+            UserToken test = new UserToken("test", "password");
+            //Make sure it is at least generating something
+            Assert.IsNotNull(test.Salt);
+            //Set to a specific salt so it's testable
+            test.Salt = "abcdef";
+            test.UpdatePassword("password");
+
+            string testString = test.ToString();
+            string expected = "u=test&t=9d8d1f345f220fec61ae3ff604416ff6&s=abcdef";
+            Assert.AreEqual(expected, testString);
+        }
+    }
+}
